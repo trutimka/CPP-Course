a9b5c591c9099c81f5fcd24b8a73f6909ca3f9f1
trutimka <trutimka3105@gmail.com>
2022-10-19 18:04:25 +0300

    add solution for 2d_spiral

--- 2d-spiral/2d_spiral.hpp
+++ 2d-spiral/2d_spiral.hpp
@@ -3,7 +3,11 @@ int** MakeSpiral(int n) {
   for (int i = 0; i < n; ++i) {
     spiral[i] = new int[n];
   }
-  int row1 = 0, col1 = 0, row2 = n - 1, col2 = n - 1, iter = 1;
+  int row1 = 0;
+  int col1 = 0;
+  int row2 = n - 1;
+  int col2 = n - 1;
+  int iter = 1;
   while (iter <= (n * n)) {
     for (int i = col1; i <= col2; i++) {
       spiral[row1][i] = iter++;
