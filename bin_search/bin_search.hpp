1945f910c9d3b2e52f2b26a9904018cd43929ee9
trutimka <trutimka3105@gmail.com>
2022-10-25 17:51:29 +0300

    add solution of bin_search

--- bin_search/bin_search.hpp
+++ bin_search/bin_search.hpp
@@ -1,6 +1,17 @@
 #pragma once
 
-int* LowerBound(int* /* first */, int* /* last */, int /* value */) {
-    /* Not Implemented */
-    return nullptr;
+int* LowerBound(int* first, int* last, int value) {
+  int dist = last - first;
+  int* mid = first + dist / 2;
+  while (first != last) {
+    if (*mid < value) {
+      first = ++mid;
+    }
+    else {
+      last = mid;
+    }
+    dist = last - first;
+    mid = first + dist / 2;
+  }
+  return first;
 }
