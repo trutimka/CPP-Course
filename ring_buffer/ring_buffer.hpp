73535ebd69b94d9613e211a1b1da08eed8d07680
trutimka <trutimka3105@gmail.com>
2022-10-30 12:11:39 +0300

    add solution of ring buffer

--- ring_buffer/ring_buffer.hpp
+++ ring_buffer/ring_buffer.hpp
@@ -1,25 +1,51 @@
 #pragma once
 
 #include <cstddef>
+#include <vector>
 
 class RingBuffer {
- public:
+public:
   explicit RingBuffer(size_t capacity) {
+    size_ = capacity;
+    buffer_.resize(capacity);
   }
 
-  size_t Size() const {
-    return 0;
-  }
+  size_t Size() const { return cur_size_; }
 
-  bool Empty() const {
+  bool Empty() const { 
+    if (cur_size_ == 0) return true;
     return false;
   }
 
   bool TryPush(int element) {
-    return true;
+    if (cur_size_ == size_) {
+      return false;
+    }
+    else {
+      buffer_[end_] = element;
+      ++end_;
+      end_ %= size_;
+      ++cur_size_;
+      return true;
+    }
   }
 
   bool TryPop(int* element) {
-    return true;
+    if (cur_size_ != 0) {
+      *element = buffer_[begin_];
+      ++begin_;
+      begin_ %= size_;
+      --cur_size_;
+      return true;
+    }
+    else {
+      return false;
+    }
   }
+private:
+  std::vector<int> buffer_;
+  size_t begin_ = 0;
+  size_t end_ = 0;
+  size_t size_ = 0;
+  size_t cur_size_ = 0;
 };
