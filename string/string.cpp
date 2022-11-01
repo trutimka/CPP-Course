93972c47a74c75b9dc7480fa12812eee9a61413a
trutimka <trutimka3105@gmail.com>
2022-11-01 20:10:50 +0300

    add solution of string

--- string/string.cpp
+++ string/string.cpp
@@ -158,18 +158,21 @@ std::istream& operator>>(std::istream& in, String& str) {
 }
 
 String operator*(String k_first, int n) {
-  String str;
-  str.size_ = k_first.size_ * n;
-  str.capacity_ = str.size_;
-  str.data_ = new char[str.size_ + 1];
-  memcpy(str.data_, k_first.data_, k_first.size_);
-  for (int i = 0; i < n - 1; ++i) {
-    for (size_t j = 0; j < k_first.size_; ++j) {
-      str.data_[k_first.size_ * (i + 1) + j] = k_first.data_[j];
+  if (n != 0) {
+    String str;
+    str.size_ = k_first.size_ * n;
+    str.capacity_ = str.size_;
+    str.data_ = new char[str.size_ + 1];
+    memcpy(str.data_, k_first.data_, k_first.size_);
+    for (int i = 0; i < n - 1; ++i) {
+      for (size_t j = 0; j < k_first.size_; ++j) {
+        str.data_[k_first.size_ * (i + 1) + j] = k_first.data_[j];
+      }
     }
+    str.data_[str.size_] = '\0';
+    return str;
   }
-  str.data_[str.size_] = '\0';
-  return str;
+  return String();
 }
 
 String operator*=(String& k_first, int n) {
