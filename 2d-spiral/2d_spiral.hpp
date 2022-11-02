80b968fe2fd25f46a3c7533331cdb6135e7d01d8
trutimka <trutimka3105@gmail.com>
2022-11-02 04:59:29 +0300

    add a new version of solution

--- 2d-spiral/2d_spiral.hpp
+++ 2d-spiral/2d_spiral.hpp
@@ -3,28 +3,24 @@ int** MakeSpiral(int n) {
   for (int i = 0; i < n; ++i) {
     spiral[i] = new int[n];
   }
-  int row_start = 0;
-  int col_start = 0;
-  int row_finish = n - 1;
-  int col_finish = n - 1;
+  int start = 0;
+  int finish = n - 1;
   int temp_num = 1;
   while (temp_num <= n * n) {
-    for (int i = col_start; i <= col_finish; ++i) {
-      spiral[row_start][i] = temp_num++;
+    for (int i = start; i <= finish; ++i) {
+      spiral[start][i] = temp_num++;
     }
-    for (int j = row_start + 1; j <= row_finish; ++j) {
-      spiral[j][col_finish] = temp_num++;
+    for (int j = start + 1; j <= finish; ++j) {
+      spiral[j][finish] = temp_num++;
     }
-    for (int i = col_finish - 1; i >= col_start; --i) {
-      spiral[row_finish][i] = temp_num++;
+    for (int i = finish - 1; i >= start; --i) {
+      spiral[finish][i] = temp_num++;
     }
-    for (int j = row_finish - 1; j >= row_start + 1; --j) {
-      spiral[j][col_start] = temp_num++;
+    for (int j = finish - 1; j >= start + 1; --j) {
+      spiral[j][start] = temp_num++;
     }
-    row_start++;
-    row_finish--;
-    col_start++;
-    col_finish--;
+    finish--;
+    start++;
   }
   return spiral;
 }
