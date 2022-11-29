2146ad46edf6cb4e6b9b7b3999b488e45fc079fe
Тимофей <trutimka3105@gmail.com>
2022-11-29 21:26:03 +0300

    Update string.hpp

--- string/string.hpp
+++ string/string.hpp
@@ -32,44 +32,15 @@ class String {
   size_t Size() const;
   size_t Capacity() const;
   const char* Data() const;
-  bool operator<(const String& right) const {
-    return strcmp(data_, right.data_) < 0;
-  }
-  bool operator<=(const String& right) const {
-    return strcmp(data_, right.data_) <= 0;
-  }
-  bool operator>(const String& right) const { return !(*this <= right); }
-  bool operator>=(const String& right) const { return !(*this < right); }
-  bool operator==(const String& right) const {
-    if (size_ == right.size_) {
-      for (size_t i = 0; i < size_; ++i) {
-        if (data_[i] != right[i]) {
-          return false;
-        }
-      }
-      return true;
-    }
-    return false;
-  }
-  bool operator!=(const String& right) const { return !(*this == right); }
+  bool operator<(const String& right) const;
+  bool operator<=(const String& right) const;
+  bool operator>(const String& right) const;
+  bool operator>=(const String& right) const;
+  bool operator==(const String& right) const;
+  bool operator!=(const String& right) const;
 
   friend String operator+(String k_first, String k_second);
-  String& operator+=(String k_second) {
-    size_t temp_size = size_;
-    size_ += k_second.size_;
-    if (size_ >= capacity_) {
-      char* temp_data = data_;
-      data_ = new char[size_ + 1];
-      capacity_ = size_;
-      memcpy(data_, temp_data, temp_size);
-      delete[] temp_data;
-    }
-    for (size_t i = 0; i < k_second.size_; ++i) {
-      data_[i + temp_size] = k_second.data_[i];
-    }
-    data_[size_] = '\0';
-    return *this;
-  }
+  String& operator+=(String k_second);
   friend String operator*(String k_first, int n);
   friend String operator*=(String& k_first, int n);
   friend std::ostream& operator<<(std::ostream& out, const String& str);
